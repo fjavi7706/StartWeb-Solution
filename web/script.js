71e6fbf1466d26bb844bec1301c0e5a276ef892a
fjavi7706 <fjavi7706@gmail.com>
2026-05-27 10:57:35 +0200

    feat: añadir sección de proyectos

--- web/script.js
+++ web/script.js
@@ -18,3 +18,38 @@ L.marker([LATITUD, LONGITUD])
     .addTo(map)
     .bindPopup(` <strong>${NOMBRE_INSTITUTO}</strong><br> Nuestro centro educativo`)
     .openPopup();  // Abre el popup automáticamente al cargar
+
+function actualizarHora() {
+     // Crear objeto con la fecha y hora actual
+    let ahora = new Date();
+
+     // Obtener día, mes, año
+    let dia = ahora.getDate();
+    let mes = ahora.getMonth() + 1; 
+    let año = ahora.getFullYear();
+
+      // Obtener hora, minuto, segundo
+    let horas = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
+
+    // Añadir cero si es menor a 10
+    if (horas < 10) horas = "0" + horas;
+    if (minutos < 10) minutos = "0" + minutos;
+    if (segundos < 10) segundos = "0" + segundos;
+
+     // Nombres de los días y meses (para la fecha bonita)
+    let dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    
+    let nombreDia = dias[ahora.getDay()];
+    let nombreMes = meses[ahora.getMonth()];
+
+     // Formatear la fecha bonita: "Lunes ** de Mayo de 2026"
+    let fechaBonita = nombreDia + " " + dia + " de " + nombreMes + " de " + año;
+    
+    // Formatear la hora: "14:35:22"
+    let horaBonita = horas + ":" + minutos + ":" + segundos;
+}
+
+setInterval(actualizarHora, 1000);
